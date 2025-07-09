86df7ec2a7cb76ab1d03683e35c10d65f9608ac1
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-09 12:50:10 -0300

    Inserçao de usuario com POST

--- src/main/java/br/com/leonardogtc/resources/UserResource.java
+++ src/main/java/br/com/leonardogtc/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.leonardogtc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.leonardogtc.domain.User;
 import br.com.leonardogtc.dto.UserDTO;
@@ -34,6 +38,15 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User user = service.fromDTO(objDto);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(null).build();
 	}
 
 }
--- src/main/java/br/com/leonardogtc/services/UserService.java
+++ src/main/java/br/com/leonardogtc/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.leonardogtc.domain.User;
+import br.com.leonardogtc.dto.UserDTO;
 import br.com.leonardogtc.repository.UserRepository;
 import br.com.leonardogtc.services.exception.ObjectNotFoundException;
 
@@ -30,5 +31,13 @@ public class UserService {
 	public User findById(String id) {
 		return repo.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id + ", Type: " + User.class.getName()));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
