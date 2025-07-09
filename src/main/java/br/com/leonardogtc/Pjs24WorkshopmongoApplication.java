9cab21736ad4f55f258a4f9ecce7c2e730789ab5
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-08 22:20:09 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/br/com/leonardogtc/Pjs24WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardogtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pjs24WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pjs24WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leonardogtc/Pjs24WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardogtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pjs24WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
