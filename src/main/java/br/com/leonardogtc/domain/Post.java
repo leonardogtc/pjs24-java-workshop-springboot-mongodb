5c5403bf1a8d4290d425a0429699a0743c62bee7
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-10 10:50:04 -0300

    Projeçao dos dados do Author com DTO

--- src/main/java/br/com/leonardogtc/domain/Post.java
+++ src/main/java/br/com/leonardogtc/domain/Post.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import br.com.leonardogtc.dto.AuthorDTO;
+
 @Document
 public class Post implements java.io.Serializable {
 	
@@ -15,12 +17,12 @@ public class Post implements java.io.Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author; // Assuming you might want to add an author field later
+	private AuthorDTO author; // Assuming you might want to add an author field later
 	
 	public Post() {
 	}
 	
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -60,11 +62,11 @@ public class Post implements java.io.Serializable {
 		this.body = body;
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
