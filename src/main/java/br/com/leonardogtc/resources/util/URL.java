f2f276aa33b228867125ce2981637dca771d62d3
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-10 23:20:09 -0300

    Consulta com varios criterios

--- src/main/java/br/com/leonardogtc/resources/util/URL.java
+++ src/main/java/br/com/leonardogtc/resources/util/URL.java
@@ -1,9 +1,23 @@
 package br.com.leonardogtc.resources.util;
 
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	public static String decodeParam(String text) {
 		return URLDecoder.decode(text, java.nio.charset.StandardCharsets.UTF_8);
+	}
+	
+	public static Date convertDate(String text, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT-3")); // Adjust to your timezone if necessary
+		try {
+			return sdf.parse(text);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
 	}
 }
--- src/main/java/br/com/leonardogtc/services/PostService.java
+++ src/main/java/br/com/leonardogtc/services/PostService.java
@@ -1,5 +1,6 @@
 package br.com.leonardogtc.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 //		return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // Adjust maxDate to include the whole day
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
